d3adcf50a15f714d8c4c074aa7e90f7e6324dce4
Zamperlini <monkeydluffysan2021@gmail.com>
2023-02-14 22:46:23 -0300

    otimização

--- js/app.js
+++ js/app.js
@@ -1,25 +1,21 @@
-$(document).ready(function () {
-  var classActive = "active";
-  $(".animais .tab-menu a").first().addClass(classActive);
-  $(".animais .item").first().addClass(classActive);
+$("[data-group]").each(function () {
+  const $allTarget = $(this).find("[data-target]"),
+    $allClick = $(this).find("[data-click]"),
+    classActive = "active";
 
-  $(".animais .tab-menu a").click(function (e) {
+  $allClick.first().addClass(classActive);
+  $allTarget.first().addClass(classActive);
+
+  $allClick.click(function (e) {
     e.preventDefault();
-    var itemId = $(this).attr("href");
 
-    $(".animais .tab-menu a,.animais .item").removeClass(classActive);
+    const id = $(this).data("click"),
+      $target = $('[data-target="' + id + '"]');
+
+    $allClick.removeClass(classActive);
+    $allTarget.removeClass(classActive);
+
+    $target.addClass(classActive);
     $(this).addClass(classActive);
-    $(itemId).addClass(classActive);
-  });
-  $(".florestas .tab-menu a").first().addClass(classActive);
-  $(".florestas .item").first().addClass(classActive);
-
-  $(".florestas .tab-menu a").click(function (e) {
-    e.preventDefault();
-    var itemId = $(this).attr("href");
-
-    $(".florestas .tab-menu a,.florestas .item").removeClass(classActive);
-    $(this).addClass(classActive);
-    $(itemId).addClass(classActive);
   });
 });
